af5cdf588497ebac4c070272389d6e5b743d1a68
dentopiasa <castelli_philippe@hotmail.com>
2023-11-05 12:59:44 +0200

    button selection

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
+const homeBtn = document.querySelectorAll("#home-btn")
+const messageBox = document.getElementById("message-box")
 const allTasks = document.querySelector(".allTasks");
 const today = document.querySelector(".today");
 const nextSeven = document.querySelector(".next-seven");
 const important = document.querySelector(".important");
 
-const btnArray = {allTasks, today, nextSeven, important};
-console.log(btnArray);
-
-btnArray.forEach(e) {
-    console.log(e.target);
-});
-
-/*allTasks.addEventListener("click", ()=> {
-    allTasks.classList.add("selection-bg");
-})*/
+for (let i = 0; i < homeBtn.length; i++) {
+    homeBtn[i].addEventListener("click", function () {
+      homeBtn.forEach(el=> el.classList.remove("selection-bg"))
+      homeBtn[i].classList.add("selection-bg");
+      messageBox.innerHTML = homeBtn[i].textContent;
+    });
+  }
