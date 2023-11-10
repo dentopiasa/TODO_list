173803b09a663244b755962bdefdebb7872b54a3
dentopiasa <castelli_philippe@hotmail.com>
2023-11-10 18:18:16 +0200

    so stuck

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 //Highlight Home Button and display on green box
 const homeBtn = document.querySelectorAll("#home-btn");
 const messageBox = document.getElementById("message-box"); 
-for (let i = 0; i < homeBtn.length; i++) {
+var arr = Array.from(homeBtn);
+
+function refresh() {
+for (let i = 0; i < arr.length; i++) {
+  arr[i].addEventListener("click", function () {
+    arr.forEach(el=> el.classList.remove("selection-bg"))
+    arr[i].classList.add("selection-bg");
+    messageBox.innerHTML = arr[i].textContent;
+  });
+}
+}
+/*for (let i = 0; i < homeBtn.length; i++) {
     homeBtn[i].addEventListener("click", function () {
       homeBtn.forEach(el=> el.classList.remove("selection-bg"))
       homeBtn[i].classList.add("selection-bg");
       messageBox.innerHTML = homeBtn[i].textContent;
     });
-  }
+  }*/
+
 //Add Project Main Menu
 document.querySelector(".addProject").addEventListener("click", ()=> {
   document.querySelector(".project-input").style.display = "flex";
@@ -17,6 +29,19 @@ document.getElementById("cancelProjectBtn").addEventListener("click", ()=> {
 })
 
 document.getElementById("addProjectBtn").addEventListener("click", ()=> {
-  const projectName = document.getElementById("project-name");
-  alert(projectName.value);
+  
+    const projectName = document.getElementById("project-name");
+    const newProject = document.createElement("div");
+    const project = document.querySelector(".new-project");
+    const node = document.createTextNode(projectName.value);
+
+    newProject.setAttribute("id", "home-btn");
+    newProject.appendChild(node);
+    newProject.classList.add("btnStyle")
+    project.appendChild(newProject);
+    arr.push(newProject);
+    console.log(arr);
+    console.log(homeBtn);
+    console.log(newProject)
 })
+refresh();
